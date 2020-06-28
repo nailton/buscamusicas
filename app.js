f10104182b103be40145a1522233b13179cc7047
Nailton Sousa <nailton.osn@gmail.com>
2020-06-27 21:16:58 -0300

    get Lyrics

--- app.js
+++ app.js
@@ -48,3 +48,28 @@ form.addEventListener('submit', event => {
 
   fetchSongs(searchTerm)
 })
+
+const fetchLytics = async (artist, songTitle) => {
+ const response = await fetch(`${apiURL}/v1/${artist}/${songTitle}`)
+ const data = await response.json()
+ const lyrics = data.lyrics.replace(/(\r\n|\r|\n)/g, '<br>')
+
+ songsContainer.innerHTML = `
+ <li class="lyrics-container">
+  <h2><strong>${songTitle}</strong> - ${artist}</h2>
+  <p class="lytics">${lyrics}</p>
+ </li>
+ `
+}
+
+songsContainer.addEventListener('click', event => {
+  const clickedElement = event.target
+
+  if (clickedElement.tagName === 'BUTTON') {
+    const artist = clickedElement.getAttribute('data-artist')
+    const songTitle = clickedElement.getAttribute('data-song-title')
+
+    prevAndNextContainer.innerHTML = ''
+    fetchLytics(artist, songTitle)
+  }
+})
